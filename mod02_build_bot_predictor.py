bb91e45bf870ad5785ba015c5d584efd3b27a6a6
Michael Lombardi <michael@Michaels-MacBook-Pro.local>
2026-09-17 22:02:46 -0400

    module 2 hw upload 1

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -10,11 +10,11 @@ def train_model(X, y, seed=seed):
     Build a GBM on given data
     """
     model = GradientBoostingClassifier(
-        learning_rate=0.1,
-        n_estimators=100,
-        max_depth=8,
-        subsample=1,
-        min_samples_leaf=1,
+        learning_rate=0.01,
+        n_estimators=300,
+        max_depth=2,
+        subsample=0.5,
+        min_samples_leaf=2,
         random_state=seed
     )
     model.fit(X, y)
